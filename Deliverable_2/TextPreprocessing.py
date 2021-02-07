a336affb205618653bd9998d5ad33ebc30d697bc
camillebeguin <camillebeguin@Camilles-MacBook-Pro.local>
2021-02-07 11:51:58 +0100

    add lemmatizer and updated notebook

--- Deliverable_2/TextPreprocessing.py
+++ Deliverable_2/TextPreprocessing.py
@@ -1,9 +1,17 @@
 #import libraries 
 import pandas as pd 
 import numpy as np 
+
+# nltk functions
+import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer, PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+nltk.download('averaged_perceptron_tagger')
+
+# other textpreprocessing functions
 import re
 import string
 import cld2
@@ -121,7 +129,7 @@ class TextPreprocessor:
         # call stopword remover
         self.corpus = self.stopword_remover()
 
-# STEMMING/LEMMATIZING TEXT
+# STEMMING TEXT
 def stem_corpus(corpus, stemmer_type="Lancaster"): 
     '''
         # stemmer type: Lancaster or Porter
@@ -135,3 +143,41 @@ def stem_corpus(corpus, stemmer_type="Lancaster"):
         for j in range(len(corpus[i])):
             corpus[i][j] = stemmer.stem(corpus[i][j])
     return corpus
+
+# LEMMATIZING TEXT
+class LemmatizeCorpus:
+
+    def __init__(self, corpus):
+        self.lemmatizer = WordNetLemmatizer()
+        self.corpus = corpus
+
+    def nltk2wn_tag(self, nltk_tag):
+        if nltk_tag.startswith('J'):
+            return wordnet.ADJ
+        elif nltk_tag.startswith('V'):
+            return wordnet.VERB
+        elif nltk_tag.startswith('N'):
+            return wordnet.NOUN
+        elif nltk_tag.startswith('R'):
+            return wordnet.ADV
+        else:                    
+            return None
+
+    def lemmatize_sentence(self, sentence):
+        sentence = sentence.copy()
+        sentence = " ".join(sentence)
+        nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))    
+        wn_tagged = map(lambda x: (x[0], self.nltk2wn_tag(x[1])), nltk_tagged)
+        res_words = []
+        for word, tag in wn_tagged:
+            if tag is None:                        
+                res_words.append(word)
+            else:
+                res_words.append(self.lemmatizer.lemmatize(word, tag))
+
+        return res_words
+
+    def lemmatize_corpus(self):
+        lemmatized_corpus = [self.lemmatize_sentence(sentence) for sentence in self.corpus]
+        return lemmatized_corpus
+
